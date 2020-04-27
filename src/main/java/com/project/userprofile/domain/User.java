e260e4351ad207f9524c487f0b5181d5868b0813
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-04-27 14:57:52 +0100

    Main code for user entity

--- src/main/java/com/project/userprofile/domain/User.java
+++ src/main/java/com/project/userprofile/domain/User.java
@@ -1,5 +1,47 @@
 package com.project.userprofile.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Entity(name = "user_profile")
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
 
+    @Id
+    private long id;
+
+    @Column(name = "email_address")
+    @Size(max = 255)
+    private String email;
+
+    @Column(name = "first_name")
+    @Size(max = 255)
+    private String firstName;
+
+    @Column(name = "last_name")
+    @Size(max = 255)
+    private String lastName;
+
+    @Column(name = "created")
+    @CreationTimestamp
+    private LocalDateTime created;
+
+    @Column(name = "last_updated")
+    @UpdateTimestamp
+    private LocalDateTime lastUpdated;
+
+    public User(String email, String firstName, String lastName) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
