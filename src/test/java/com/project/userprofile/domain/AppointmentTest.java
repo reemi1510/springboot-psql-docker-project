405ea806883f2056a5957f2dfdfb51f2de6f6cea
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-04-27 17:06:10 +0100

    Refactored appointment entity test code

--- src/test/java/com/project/userprofile/domain/AppointmentTest.java
+++ src/test/java/com/project/userprofile/domain/AppointmentTest.java
@@ -1,18 +1,32 @@
 package com.project.userprofile.domain;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class AppointmentTest {
 
-    @Test
-    public void user_has_required_fields() {
-        Appointment appointment = new Appointment();
-        assertThat(appointment).hasFieldOrProperty("id");
-        assertThat(appointment).hasFieldOrProperty("user");
-        assertThat(appointment).hasFieldOrProperty("roleId");
-        assertThat(appointment).hasFieldOrProperty("roleDesc");
-        assertThat(appointment).hasFieldOrProperty("orgName");
+    private long roleId = 1;
+    private String roleDesc = "Developer";
+    private String orgName = "Test Org";
+    User mockUser;
+    Appointment appointment;
+
+    @Before
+    public void setUp() {
+        mockUser = mock(User.class);
+        appointment = new Appointment(roleId, roleDesc, orgName, mockUser);
     }
+
+    @Test
+    public void creates_appointment_correctly() {
+        assertThat(appointment.getRoleId()).isEqualTo(roleId);
+        assertThat(appointment.getRoleDesc()).isEqualTo(roleDesc);
+        assertThat(appointment.getOrgName()).isEqualTo(orgName);
+        assertThat(appointment.getUser()).isEqualTo(mockUser);
+        assertThat(appointment.getId()).isZero();
+    }
+
 }
