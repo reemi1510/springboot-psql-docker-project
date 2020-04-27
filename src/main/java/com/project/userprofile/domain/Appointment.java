6e2bd247f7d7e0c9711cc24df4974b6d092f8573
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-04-27 15:14:39 +0100

    Main code for appointment entity

--- src/main/java/com/project/userprofile/domain/Appointment.java
+++ src/main/java/com/project/userprofile/domain/Appointment.java
@@ -1,5 +1,40 @@
 package com.project.userprofile.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name = "appointments")
 public class Appointment {
 
+    @Id
+    private long id;
+
+    @Column(name = "role_id")
+    private long roleId;
+
+    @Column(name = "role_description")
+    @Size(max = 255)
+    private String roleDesc;
+
+    @Column(name = "organisation_name")
+    @Size(max = 255)
+    private String orgName;
+
+    @ManyToOne
+    @JoinColumn(name = "user_profile_id")
+    private User user;
+
+    public Appointment(long roleId, String roleDesc, String orgName, User user) {
+        this.roleId = roleId;
+        this.roleDesc = roleDesc;
+        this.orgName = orgName;
+        this.user = user;
+    }
 }
