a9f7553453fa39939ee949e4c980ee6586726e7a
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-04-27 16:01:46 +0100

    Added field for appointments in user entity

--- src/main/java/com/project/userprofile/domain/User.java
+++ src/main/java/com/project/userprofile/domain/User.java
@@ -4,11 +4,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity(name = "user_profile")
 @NoArgsConstructor
@@ -38,6 +45,10 @@ public class User {
     @Column(name = "last_updated")
     @UpdateTimestamp
     private LocalDateTime lastUpdated;
+
+    @Fetch(FetchMode.SUBSELECT)
+    @OneToMany(mappedBy = "user")
+    private List<Appointment> appointments = new ArrayList<>();
 
     public User(String email, String firstName, String lastName) {
         this.email = email;
