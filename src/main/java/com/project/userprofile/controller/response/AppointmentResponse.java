0976e1b37fd4f0836063471a58b907b04ab5642c
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-04-28 11:45:53 +0100

    Main code for appointment response - fields

--- src/main/java/com/project/userprofile/controller/response/AppointmentResponse.java
+++ src/main/java/com/project/userprofile/controller/response/AppointmentResponse.java
@@ -1,4 +1,17 @@
 package com.project.userprofile.controller.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
 public class AppointmentResponse {
+
+    @JsonProperty("roleId")
+    private long roleId;
+
+    @JsonProperty("roleDescription")
+    private String roleDesc;
+
+    @JsonProperty("organisationName")
+    private String orgName;
 }
