3a2f53ddc11eded0bb6657824c942ed18d746a63
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-04-28 14:27:46 +0100

    Refactored test for appointment response

--- src/test/java/com/project/userprofile/controller/response/AppointmentResponseTest.java
+++ src/test/java/com/project/userprofile/controller/response/AppointmentResponseTest.java
@@ -1,19 +1,25 @@
 package com.project.userprofile.controller.response;
 
+import com.project.userprofile.domain.Appointment;
+import com.project.userprofile.domain.User;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppointmentResponseTest {
 
-    AppointmentResponse appointmentResponse;
-
     @Test
-    public void has_required_fields() {
-        appointmentResponse = new AppointmentResponse();
+    public void creates_appointment_response_correctly() {
+        final long expectedRoleId = 1;
+        final String expectedRoleDesc = "Developer";
+        final String expectedOrgName = "Test Org";
+        final User user = new User();
+        final Appointment appointment = new Appointment(1, "Developer", "Test Org", user);
 
-        assertThat(appointmentResponse).hasFieldOrProperty("roleId");
-        assertThat(appointmentResponse).hasFieldOrProperty("roleDesc");
-        assertThat(appointmentResponse).hasFieldOrProperty("orgName");
+        AppointmentResponse appointmentResponse = new AppointmentResponse(appointment);
+
+        assertThat(appointmentResponse.getRoleId()).isEqualTo(expectedRoleId);
+        assertThat(appointmentResponse.getRoleDesc()).isEqualTo(expectedRoleDesc);
+        assertThat(appointmentResponse.getOrgName()).isEqualTo(expectedOrgName);
     }
 }
