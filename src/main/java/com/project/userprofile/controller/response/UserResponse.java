b734a2ff16a8234233a4106f1b80623795c9d73f
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-04-28 15:11:29 +0100

    Main code for user response - fields

--- src/main/java/com/project/userprofile/controller/response/UserResponse.java
+++ src/main/java/com/project/userprofile/controller/response/UserResponse.java
@@ -1,4 +1,22 @@
 package com.project.userprofile.controller.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
 public class UserResponse {
+
+    @JsonProperty
+    private String email;
+
+    @JsonProperty
+    private String firstName;
+
+    @JsonProperty
+    private String lastName;
+
+    @JsonProperty
+    private List<AppointmentResponse> appointments;
 }
