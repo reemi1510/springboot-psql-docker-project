7e80c1c076248ab41317a1e29b278f11e0dbd28b
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-04-28 15:31:09 +0100

    Refactored test for user response

--- src/test/java/com/project/userprofile/controller/response/UserResponseTest.java
+++ src/test/java/com/project/userprofile/controller/response/UserResponseTest.java
@@ -1,19 +1,29 @@
 package com.project.userprofile.controller.response;
 
+import com.project.userprofile.domain.Appointment;
+import com.project.userprofile.domain.User;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserResponseTest {
 
-    UserResponse userResponse;
-
     @Test
-    public void has_required_fields() {
-        userResponse = new UserResponse();
-        assertThat(userResponse).hasFieldOrProperty("email");
-        assertThat(userResponse).hasFieldOrProperty("firstName");
-        assertThat(userResponse).hasFieldOrProperty("lastName");
-        assertThat(userResponse).hasFieldOrProperty("appointments");
+    public void creates_user_response_correctly() {
+        final String expectedEmail = "example@test.com";
+        final String expectedFirstName = "Jane";
+        final String expectedLastName = "Doe";
+
+        User user = new User(expectedEmail, expectedFirstName, expectedLastName);
+        Appointment appointment = new Appointment(1, "Dev", "Org", user);
+        user.addAppointment(appointment);
+
+        UserResponse userResponse = new UserResponse(user);
+
+        assertThat(userResponse.getEmail()).isEqualTo(expectedEmail);
+        assertThat(userResponse.getFirstName()).isEqualTo(expectedFirstName);
+        assertThat(userResponse.getLastName()).isEqualTo(expectedLastName);
+        assertThat(userResponse.getAppointments().size()).isEqualTo(1);
+
     }
 }
