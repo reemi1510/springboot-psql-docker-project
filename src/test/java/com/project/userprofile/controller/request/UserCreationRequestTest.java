60b0b981a59932cd519ffa48bde29b8a95814bec
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-04-28 10:40:38 +0100

    Refactored test for user creation request

--- src/test/java/com/project/userprofile/controller/request/UserCreationRequestTest.java
+++ src/test/java/com/project/userprofile/controller/request/UserCreationRequestTest.java
@@ -2,18 +2,27 @@ package com.project.userprofile.controller.request;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserCreationRequestTest {
 
     UserCreationRequest userCreationRequest;
+    private String email = "example@test.com";
+    private String firstName = "Jane";
+    private String lastName = "Doe";
+    private List<AppointmentCreationRequest> appointments = new ArrayList<>();
+    private AppointmentCreationRequest appointment = new AppointmentCreationRequest(1, "Dev", "Test");
 
     @Test
-    public void has_required_fields() {
-        userCreationRequest = new UserCreationRequest();
-        assertThat(userCreationRequest).hasFieldOrProperty("email");
-        assertThat(userCreationRequest).hasFieldOrProperty("firstName");
-        assertThat(userCreationRequest).hasFieldOrProperty("lastName");
-        assertThat(userCreationRequest).hasFieldOrProperty("appointments");
+    public void creates_user_request_correctly() {
+        appointments.add(appointment);
+        userCreationRequest = new UserCreationRequest(email, firstName, lastName,appointments);
+        assertThat(userCreationRequest.getEmail()).isEqualTo(email);
+        assertThat(userCreationRequest.getFirstName()).isEqualTo(firstName);
+        assertThat(userCreationRequest.getLastName()).isEqualTo(lastName);
+        assertThat(userCreationRequest.getAppointments()).contains(appointment);
     }
 }
