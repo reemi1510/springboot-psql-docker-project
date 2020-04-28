29a510504d5a7869adde87be345e9820d921b346
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-04-28 09:59:23 +0100

    Main code for appointment creation request

--- src/main/java/com/project/userprofile/controller/request/AppointmentCreationRequest.java
+++ src/main/java/com/project/userprofile/controller/request/AppointmentCreationRequest.java
@@ -1,4 +1,28 @@
 package com.project.userprofile.controller.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class AppointmentCreationRequest {
+
+    private long roleId;
+
+    private String roleDesc;
+
+    private String orgName;
+
+    @JsonCreator
+    public AppointmentCreationRequest(
+            @JsonProperty("roleId") long roleId,
+            @JsonProperty("roleDescription") String roleDesc,
+            @JsonProperty("organisationName") String orgName) {
+
+        this.roleId = roleId;
+        this.roleDesc = roleDesc;
+        this.orgName = orgName;
+    }
 }
