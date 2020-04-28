2b2508822258a3b50e1ff1437ef28401513de966
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-04-28 15:13:39 +0100

    Main code for user response - constructor

--- src/main/java/com/project/userprofile/controller/response/UserResponse.java
+++ src/main/java/com/project/userprofile/controller/response/UserResponse.java
@@ -1,22 +1,35 @@
 package com.project.userprofile.controller.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.project.userprofile.domain.User;
 import lombok.Getter;
 
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Getter
 public class UserResponse {
 
     @JsonProperty
-    private String email;
+    private final String email;
 
     @JsonProperty
-    private String firstName;
+    private final String firstName;
 
     @JsonProperty
-    private String lastName;
+    private final String lastName;
 
     @JsonProperty
-    private List<AppointmentResponse> appointments;
+    private final List<AppointmentResponse> appointments;
+
+    public UserResponse(User user) {
+        this.email = user.getEmail();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.appointments = user.getAppointments()
+                .stream()
+                .map(appointment -> new AppointmentResponse(appointment))
+                .collect(toList());
+    }
 }
