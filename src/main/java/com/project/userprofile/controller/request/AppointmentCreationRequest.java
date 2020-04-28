22e4c237f8040d809786b03f576b50f5d18a7f0d
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-04-28 09:32:36 +0100

    Main code for appointment creation request

--- src/main/java/com/project/userprofile/controller/request/AppointmentCreationRequest.java
+++ src/main/java/com/project/userprofile/controller/request/AppointmentCreationRequest.java
@@ -1,4 +1,10 @@
 package com.project.userprofile.controller.request;
 
 public class AppointmentCreationRequest {
+
+    private long roleId;
+
+    private String roleDescription;
+
+    private String organisationName;
 }
