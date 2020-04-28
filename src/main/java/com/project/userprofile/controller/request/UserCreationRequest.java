0f512473fcf2f3dd2714dfcaf0dda32b106fa94c
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-04-28 10:28:13 +0100

    Main code for user creation request- fields

--- src/main/java/com/project/userprofile/controller/request/UserCreationRequest.java
+++ src/main/java/com/project/userprofile/controller/request/UserCreationRequest.java
@@ -1,4 +1,15 @@
 package com.project.userprofile.controller.request;
 
+import java.util.List;
+
 public class UserCreationRequest {
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private List<AppointmentCreationRequest> appointments;
+
 }
