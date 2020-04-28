fd481afb3492eddbb5d933b288ce5168a2f9593b
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-04-28 09:56:19 +0100

    Revert "Main code for appointment creation request"
    This reverts commit 22e4c237f8040d809786b03f576b50f5d18a7f0d.

--- src/main/java/com/project/userprofile/controller/request/AppointmentCreationRequest.java
+++ src/main/java/com/project/userprofile/controller/request/AppointmentCreationRequest.java
@@ -1,10 +1,4 @@
 package com.project.userprofile.controller.request;
 
 public class AppointmentCreationRequest {
-
-    private long roleId;
-
-    private String roleDescription;
-
-    private String organisationName;
 }
