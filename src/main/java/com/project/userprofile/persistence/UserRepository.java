215135261a59800099e8c75386411c8f3de05420
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-04-30 16:49:41 +0100

    Added user repository

---
+++ src/main/java/com/project/userprofile/persistence/UserRepository.java
@@ -0,0 +1,9 @@
+package com.project.userprofile.persistence;
+
+import com.project.userprofile.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
