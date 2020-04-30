64e982cc134d498f616b601674207814e2a75e7b
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-04-30 16:10:19 +0100

    Initial main code for user service

--- src/main/java/com/project/userprofile/service/UserService.java
+++ src/main/java/com/project/userprofile/service/UserService.java
@@ -1,4 +1,9 @@
 package com.project.userprofile.service;
 
+import com.project.userprofile.controller.request.UserCreationRequest;
+import com.project.userprofile.controller.response.UserResponse;
+
 public interface UserService {
+
+    UserResponse createUserFrom(UserCreationRequest request);
 }
--- src/test/java/com/project/userprofile/service/UserServiceImplTest.java
+++ src/test/java/com/project/userprofile/service/UserServiceImplTest.java
@@ -3,6 +3,7 @@ package com.project.userprofile.service;
 import com.project.userprofile.controller.request.UserCreationRequest;
 import com.project.userprofile.controller.response.UserResponse;
 import com.project.userprofile.service.impl.UserServiceImpl;
+import org.junit.Test;
 import org.mockito.InjectMocks;
 
 import java.util.ArrayList;
@@ -15,6 +16,7 @@ public class UserServiceImplTest {
     @InjectMocks
     UserServiceImpl sut;
 
+    @Test
     public void can_create_user_from_request_and_save() {
         List appointments = new ArrayList();
         UserCreationRequest request = new UserCreationRequest("test@email.com", "Jane", "Doe", appointments);
