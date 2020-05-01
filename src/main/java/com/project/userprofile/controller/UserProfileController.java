557c2f712c1aa74dea68e8f82e38d9c8e3edd8e6
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-05-01 14:31:08 +0100

    Main code for controller

--- src/main/java/com/project/userprofile/controller/UserProfileController.java
+++ src/main/java/com/project/userprofile/controller/UserProfileController.java
@@ -1,4 +1,24 @@
 package com.project.userprofile.controller;
 
+import com.project.userprofile.controller.request.UserCreationRequest;
+import com.project.userprofile.controller.response.UserResponse;
+import com.project.userprofile.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserProfileController {
+
+    @Autowired
+    protected UserService userService;
+
+    public ResponseEntity<UserResponse> registerUser(UserCreationRequest userCreationRequest) {
+
+        UserResponse userResponse = userService.createUserFrom(userCreationRequest);
+
+        return ResponseEntity
+                .status(201)
+                .body(userResponse);
+    }
 }
