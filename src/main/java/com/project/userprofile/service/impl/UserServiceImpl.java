0ba1a8a7c58dbcf3dbd49b9863224781597b0763
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-05-01 14:11:24 +0100

    Added main code to service concerning appointments

--- src/main/java/com/project/userprofile/service/impl/UserServiceImpl.java
+++ src/main/java/com/project/userprofile/service/impl/UserServiceImpl.java
@@ -1,18 +1,26 @@
 package com.project.userprofile.service.impl;
 
+import com.project.userprofile.controller.request.AppointmentCreationRequest;
 import com.project.userprofile.controller.request.UserCreationRequest;
 import com.project.userprofile.controller.response.UserResponse;
+import com.project.userprofile.domain.Appointment;
 import com.project.userprofile.domain.User;
+import com.project.userprofile.persistence.AppointmentRepository;
 import com.project.userprofile.persistence.UserRepository;
 import com.project.userprofile.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    AppointmentRepository appointmentRepository;
 
     @Override
     public UserResponse createUserFrom(UserCreationRequest request) {
@@ -21,6 +29,25 @@ public class UserServiceImpl implements UserService {
 
         User user = userRepository.save(newUser);
 
+        addAppointmentsToUser(request.getAppointments(), user);
+
         return new UserResponse(user);
+    }
+
+    private void addAppointmentsToUser(List<AppointmentCreationRequest> appointmentCreationRequests, User user) {
+
+        if (appointmentCreationRequests != null) {
+            appointmentCreationRequests.forEach(appointmentInfo -> {
+                Appointment newAppointment = new Appointment();
+                newAppointment.setRoleId(appointmentInfo.getRoleId());
+                newAppointment.setRoleDesc(appointmentInfo.getRoleDesc());
+                newAppointment.setOrgName(appointmentInfo.getOrgName());
+                newAppointment.setUser(user);
+
+                appointmentRepository.save(newAppointment);
+
+                user.addAppointment(newAppointment);
+            });
+        }
     }
 }
