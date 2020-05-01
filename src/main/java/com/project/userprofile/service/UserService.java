8952a5e1d8db3471aaf9a58524d1d0dd21b407ce
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-05-01 12:57:26 +0100

    Revert "Initial main code for user service"
    This reverts commit 64e982cc134d498f616b601674207814e2a75e7b.

--- src/main/java/com/project/userprofile/service/UserService.java
+++ src/main/java/com/project/userprofile/service/UserService.java
@@ -1,9 +1,4 @@
 package com.project.userprofile.service;
 
-import com.project.userprofile.controller.request.UserCreationRequest;
-import com.project.userprofile.controller.response.UserResponse;
-
 public interface UserService {
-
-    UserResponse createUserFrom(UserCreationRequest request);
 }
--- src/main/java/com/project/userprofile/service/impl/UserServiceImpl.java
+++ src/main/java/com/project/userprofile/service/impl/UserServiceImpl.java
@@ -1,15 +1,4 @@
 package com.project.userprofile.service.impl;
 
-import com.project.userprofile.controller.request.UserCreationRequest;
-import com.project.userprofile.controller.response.UserResponse;
-import com.project.userprofile.domain.User;
-
 public class UserServiceImpl {
-
-    public UserResponse createUserFrom(UserCreationRequest request) {
-
-        User newUser = new User(request.getEmail(), request.getFirstName(), request.getLastName());
-
-        return new UserResponse(newUser);
-    }
 }
