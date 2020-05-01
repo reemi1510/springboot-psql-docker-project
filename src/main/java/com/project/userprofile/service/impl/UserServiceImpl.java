6a389431f67a000e6e022bfb2c9613a9b66d33bb
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-05-01 13:42:57 +0100

    Initial main code for user service

--- src/main/java/com/project/userprofile/service/impl/UserServiceImpl.java
+++ src/main/java/com/project/userprofile/service/impl/UserServiceImpl.java
@@ -1,4 +1,26 @@
 package com.project.userprofile.service.impl;
 
-public class UserServiceImpl {
+import com.project.userprofile.controller.request.UserCreationRequest;
+import com.project.userprofile.controller.response.UserResponse;
+import com.project.userprofile.domain.User;
+import com.project.userprofile.persistence.UserRepository;
+import com.project.userprofile.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserResponse createUserFrom(UserCreationRequest request) {
+
+        User newUser = new User(request.getEmail(), request.getFirstName(), request.getLastName());
+
+        User user = userRepository.save(newUser);
+
+        return new UserResponse(user);
+    }
 }
