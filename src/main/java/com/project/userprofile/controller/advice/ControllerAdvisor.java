71e3b13c0670b40d7a12d4fb6d7ab5b7ac8da79f
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-05-10 18:41:19 +0100

    Added exception handling with message to satisfy test

--- src/main/java/com/project/userprofile/controller/advice/ControllerAdvisor.java
+++ src/main/java/com/project/userprofile/controller/advice/ControllerAdvisor.java
@@ -1,14 +1,19 @@
 package com.project.userprofile.controller.advice;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.Map;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @RestControllerAdvice
 public class ControllerAdvisor extends ResponseEntityExceptionHandler {
@@ -19,6 +24,15 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
         body.put("timestamp", LocalDateTime.now());
         body.put("message", "Email already registered");
 
-        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(body, BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", "Email field cannot be empty");
+
+        return new ResponseEntity<>(body, BAD_REQUEST);
     }
 }
