0c02f37355d87a39ceb63fa892578770807dfaeb
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-05-10 17:55:02 +0100

    Added validation to satisfy test

--- src/main/java/com/project/userprofile/controller/UserProfileController.java
+++ src/main/java/com/project/userprofile/controller/UserProfileController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RequestMapping(path = "api/v1/users")
@@ -18,7 +20,7 @@ public class UserProfileController {
 
     @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ResponseEntity<UserResponse> registerUser(@RequestBody UserCreationRequest userCreationRequest) {
+    public ResponseEntity<UserResponse> registerUser(@RequestBody @Valid UserCreationRequest userCreationRequest) {
 
         UserResponse userResponse = userService.createUserFrom(userCreationRequest);
 
