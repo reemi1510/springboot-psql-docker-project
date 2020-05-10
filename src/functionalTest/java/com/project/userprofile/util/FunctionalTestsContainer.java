26a577d318be76fc7b84e9d677a5baca9df299f7
Reem Ibrahim <reem.ibrahim1510@gmail.com>
2020-05-10 19:06:57 +0100

    Added comments re: use of Singleton design pattern

--- src/functionalTest/java/com/project/userprofile/util/FunctionalTestsContainer.java
+++ src/functionalTest/java/com/project/userprofile/util/FunctionalTestsContainer.java
@@ -2,19 +2,23 @@ package com.project.userprofile.util;
 
 import org.testcontainers.containers.PostgreSQLContainer;
 
+//Singleton design pattern used here.
 public class FunctionalTestsContainer extends PostgreSQLContainer<FunctionalTestsContainer> {
 
     private static final String IMAGE_VERSION = "postgres:9.6";
+
+    //Private static instance
     private static FunctionalTestsContainer container;
 
+    //Private constructor
     private FunctionalTestsContainer() {
         super(IMAGE_VERSION);
     }
 
+    //Static factory method checks static instance is null before creating instance.
     public static FunctionalTestsContainer getInstance() {
         if (container == null) {
             container = new FunctionalTestsContainer();
-            container.start();
         }
         return container;
     }
